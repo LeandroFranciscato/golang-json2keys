e23e95470a50b2afdad0fcd9be09a6f1144354e2
Leandro Franciscato <leandro_franciscato@hotmail.com>
2021-10-05 12:40:54 -0300

    correcting mock

--- mock.go
+++ mock.go
@@ -6,7 +6,7 @@ type Json2KeysMock struct {
 	mock.Mock
 }
 
-func (m Json2KeysMock) Parse(jsonStr string) (keys map[string]interface{}, err error) {
+func (m *Json2KeysMock) Parse(jsonStr string) (keys map[string]interface{}, err error) {
 	args := m.Called(jsonStr)
 	return args.Get(0).(map[string]interface{}), args.Error(1)
 }
